1ee5ea66e751aa55dd3762ff234fc2501ee83098
zyclovelife <zyclovelife@github.com>
2025-06-13 15:53:24 +0800

    以原生<script>标签插入Plausible统计代码

--- app/layout.tsx
+++ app/layout.tsx
@@ -42,7 +42,9 @@ export default function RootLayout({
         <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" rel="stylesheet" />
         {/* Plausible Analytics */}
         <script defer data-domain="palmix3.vercel.app" src="https://plausible.io/js/script.file-downloads.hash.outbound-links.pageview-props.revenue.tagged-events.js"></script>
-        <script dangerouslySetInnerHTML={{__html: `window.plausible = window.plausible || function() { (window.plausible.q = window.plausible.q || []).push(arguments) }`}} />
+        <script>
+          {`window.plausible = window.plausible || function() { (window.plausible.q = window.plausible.q || []).push(arguments) }`}
+        </script>
       </head>
       <body className={`${inter.variable} ${playfair.variable} ${caveat.variable} font-sans`}>
         <ThemeProvider attribute="class" defaultTheme="dark" enableSystem disableTransitionOnChange>

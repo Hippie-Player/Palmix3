5069e948d44b0bdf4d61517512cef889046adf19
zyclovelife <zyclovelife@github.com>
2025-06-13 16:11:19 +0800

    上传到github

--- app/layout.tsx
+++ app/layout.tsx
@@ -40,11 +40,12 @@ export default function RootLayout({
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap" rel="stylesheet" />
         <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" rel="stylesheet" />
-        {/* Plausible Analytics */}
+        {/* === Plausible Analytics 插入开始 === */}
         <script defer data-domain="palmix3.vercel.app" src="https://plausible.io/js/script.file-downloads.hash.outbound-links.pageview-props.revenue.tagged-events.js"></script>
         <script>
           {`window.plausible = window.plausible || function() { (window.plausible.q = window.plausible.q || []).push(arguments) }`}
         </script>
+        {/* === Plausible Analytics 插入结束 === */}
       </head>
       <body className={`${inter.variable} ${playfair.variable} ${caveat.variable} font-sans`}>
         <ThemeProvider attribute="class" defaultTheme="dark" enableSystem disableTransitionOnChange>
